23d3e195f8891bf7a2b1ee3fcbc4abd0706feb73
Amithajjargi123 <139846525+Amithajjargi123@users.noreply.github.com>
2023-08-20 11:15:28 +0530

    hdhd

---
+++ fullstack/frontend/languages/typescript/project/dom-static-product/src.js
@@ -0,0 +1,39 @@
+let div=document.getElementById('root')
+
+
+let myproducts=[
+    {name:'clock',image:'./1.jpg'},
+    {name:'camera',image:'./2.jpg'},
+    {name:'shirt',image:'./3.jpg'},
+    {name:'shoes',image:'./4.jpg'},
+    {name:'shoes',image:'./5.jpg'},
+    {name:'jeans',image:'./6.jpg'},
+    {name:'shirt',image:'./7.jpg'},
+    {name:'t-shirt',image:'./8.jpg'},
+    {name:'bag',image:'./9.jpg'},
+    {name:'cooker',image:'./10..jpeg'},
+    {name:'towel',image:'./11.jpg'},
+    {name:'pillow',image:'./12.jpg'},
+    {name:'mobiles',image:'./13.jpg'},
+    {name:'earbuds',image:'./14.jpg'},
+    {name:'oneplus',image:'./15.jpg'},
+    {name:'video',videos:'./1.mp4'}
+    
+    
+]
+
+function displayProducts(){
+   
+
+
+
+    myproducts.map((item)=>{
+        let image=document.createElement('img')
+        image.src=item.image
+        image.style.width="200px"
+        image.style.height="200px"
+        div.appendChild(image)
+    })
+
+    
+}
